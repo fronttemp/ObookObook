e6c296d3d26d6b08f35c521930aaeb6d993ea400
fronttemp <antglljung@naver.com>
2023-06-13 15:24:17 +0900

    :sparkles: feat: vercel proxy server 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import Counter from './components/Counter';
 
+(async () => {
+  const res = await fetch('/api/aladin', {
+    method: 'POST'
+  })
+  const data = await res.json()
+  // console.log(JSON.parse(data.replace(';', '')))
+  console.log(JSON.parse(data.replace(/\\|;/g, '')))
+})()
+
+// (async () => {
+//   const res = await fetch('/api/heropy', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       url: 'account',
+//       email: "thesecon@gmail.com",
+//       password: "********"
+//     })
+//   })
+//   const data = await res.json()
+//   console.log(JSON.parse(data.replace(';', '')))
+// })()
 
 function App() {
   return (
     <div className="container">
       <h1>My Shopping Mall</h1>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
       <Counter />
     </div>
   );
