e2d019b66769bf49711d414f57a5858da3871bc5
fronttemp <antglljung@naver.com>
2023-06-22 14:38:06 +0900

    1차 작업

--- src/pages/NewBookPage/index.tsx
+++ src/pages/NewBookPage/index.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const NewbookPage = () => {
   const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     //useEffect를 사용하여 신간 카테고리에 들어오자마자 검색 즉시실행
@@ -12,6 +13,8 @@ const NewbookPage = () => {
         const response = await axios.get(`/api/aladinItemSearch?s=ItemList&qt=ItemNewAll`)
         setBooks(response.data.item)
         console.log(response.data.item)
+
+        setLoading(false)
       } catch (error) {
         console.error('Failed to search books', error)
       }
@@ -22,19 +25,26 @@ const NewbookPage = () => {
   return (
     <>
     <h1>새로나온책</h1>
-    {books &&
-        books.map((book, index) => (
-          <div key={index}>
-            <h2>{book.title}</h2>
-            <img
-              src={book.cover.replace(/coversum/g, 'cover500')}
-              alt={book.title}
-            />
-            <p>{book.author}</p>
-            <p>{book.publisher}</p>
-            <a href={book.link}>상품 보러가기</a>
+    {loading ? <h1>loading...</h1> 
+    : 
+      <div>
+        {books &&
+          books.map((book, index) => (
+            <div key={index}>
+              <img
+                src={book.cover.replace(/coversum/g, 'cover')}
+                alt={book.title}
+              />
+              <span>{book.title}</span>
+              <p>{book.author}</p>
+              <p>{book.publisher}</p>
+              <a href={book.link}>상품 보러가기</a>
           </div>
-        ))}
+          ))}
+      </div>
+    }
+
+
     </>
   )
 }
