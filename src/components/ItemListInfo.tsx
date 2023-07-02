e16f14dc6b2dac4ef71ac628a0d35a3073b40ab3
idey <evxrafter@gmail.com>
2023-07-02 22:38:13 +0900

    chore: :wrench: 브런치 오류

--- src/components/ItemListInfo.tsx
+++ src/components/ItemListInfo.tsx
@@ -44,40 +44,37 @@ const ItemListInfo = ({ books }: ItemListInfoProps) => {
   console.log(location.pathname);
 
   return (
-      <div className = 'booksInfo'>
-        {books.filter(book => book.priceSales!==0).map((book, index) => (
-            <div key={index} className = 'bookInfo'>
-              <div className="bookInfo_box">
-                {bestSellerIndex ? (<span className = 'bestindex'> {index+1} </span>) : null}
-                <div
-                  className="bookImg"
-                  style={
-                    {backgroundImage: `url(${book.cover.replace(/coversum/g, 'cover200')})`}
-                  }
-                  onClick = {()=>moveDetailPage(book.isbn)}
-                >
-                </div>
-                <div className = 'book'>
-                  <div className="book_top">
-                    <div className = 'bookTitle' onClick = {()=>moveDetailPage(book.isbn)}>{truncate(book.title, 40)}</div>
-                    <div className = 'bookAuthor'>{book.author ? truncate(book.author, 30)+' · ' : null} {book.publisher}</div>
-                    <div className = 'bookPubDate'>{book.pubDate}</div>
-                    <div className="bookTag">{book.categoryName.split('>')[1]}</div>
-                  </div>
-                  <div className="book_bottom">
-                    <div className="bookreview"><StarFilled /> {book.customerReviewRank}</div>
-                    <div className = 'bookPrice'>{book.priceSales.toLocaleString()}원</div>
-                  </div>
-                </div>
+    <div className='booksInfo'>
+      {books.map((book, index) => (
+        <div key={index} className='bookInfo'>
+          <div className="bookInfo_box">
+            {bestSellerIndex ? <span className='bestindex'>{index + 1}</span> : null}
+            <div
+              className="bookImg"
+              style={{ backgroundImage: `url(${book.cover.replace(/coversum/g, 'cover200')})` }}
+              onClick={() => moveDetailPage(book.isbn)}
+            ></div>
+            <div className='book'>
+              <div className="book_top">
+                <div className='bookTitle' onClick={() => moveDetailPage(book.isbn)}>{truncate(book.title, 40)}</div>
+                <div className='bookAuthor'>{book.author ? truncate(book.author, 30) + ' · ' : null} {book.publisher}</div>
+                <div className="bookTag">{book.categoryName.split('>')[1]}</div>
+                <div className='bookPubDate'>{book.pubDate}</div>
               </div>
-              <div className = 'buttonBox'>
-                <AddBookCart book={book}/>
-                <AddBookPurchase className = 'addBtn' book={book}/>
+              <div className="book_bottom">
+                <div className="bookreview"><StarFilled /> {book.customerReviewRank}</div>
+                <div className='bookPrice'>{book.priceSales.toLocaleString()}원</div>
               </div>
             </div>
-        ))}
-      </div>
-  )
-}
+          </div>
+          <div className='buttonBox'>
+            <AddBookCart book={book} />
+            <AddBookPurchase book={book} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ItemListInfo;
